a18394d7328e32deea13bb39ce0d316eca5d7ddd
da-onthegrownamibia <70661241+digitize-berthatobias@users.noreply.github.com>
2023-06-12 15:52:59 +0900

    songa 1

--- src/routes/websites/+page.svelte
+++ src/routes/websites/+page.svelte
@@ -1,4 +1,4 @@
 <h1>Welcome to Websites Page</h1>
-<p>Check out the <a href="/beunlimited-club>Be UNLIMITED Book Club website</a></p>
-<p>Check out the <a href="/ichimantrees-org>Ichiman Trees website</a></p>
-<p>Check out the <a href="/relentlesscx-com>Relentless Customer Experience website</a></p> 
+<p>Check out the <a href="websites/beunlimited-club">Be UNLIMITED Book Club website</a></p>
+<p>Check out the <a href="websites/ichimantrees-org">Ichiman Trees website</a></p>
+<p>Check out the <a href="websites/relentlesscx-com">Relentless Customer Experience website</a></p>
